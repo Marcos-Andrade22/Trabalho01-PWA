c5f1a1b7a17d0e7cc3360248898ffb47e36875b4
Marcos Andrade <83091836+Marcos-Andrade22@users.noreply.github.com>
2025-04-24 19:36:14 -0300

    Aplica atualização de arquivos
    Não entendi muito bem o motivo mas vamo q vamo

--- ECommerce/obj/Debug/net9.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net9.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c786fe18cbe95c21bf4678124717df567dd52891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300d09f60040ca1e1711dd27b691502daae03cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
