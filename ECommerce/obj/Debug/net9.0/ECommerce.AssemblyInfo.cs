6f7e3c793146bd74c13298fdc753650d308d5ab6
xslxshy <blackandwhiterc@outlook.com>
2025-07-01 03:48:04 -0300

    feat: register page styling

--- ECommerce/obj/Debug/net9.0/ECommerce.AssemblyInfo.cs
+++ ECommerce/obj/Debug/net9.0/ECommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54ffd08ac5ab1d736fbc5178467d0db65b22339")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e641ee2b1636eeb12f3a272b6424193b1daa28e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
